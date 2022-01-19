d8de793898bf0b596be2c8995a88dc179f089b93
Jerry Zhu <xpost2000cod@gmail.com>
2022-01-19 14:55:39 -0500

    Opening an SDL2 window.
    Okay time to clear the tasks slowly!

--- makefile
+++ makefile
@@ -1,16 +1,19 @@
 SRCFILES=src/main.c
 CFLAGS=-Wall -Wextra -std=c11
-CLIBS=
+CLIBS=`pkg-config --libs --cflags sdl2`
+
+.phony: all clean build run
 
 all: clean build
 
 build: game.exe
+run: game.exe
+	./game.exe
+
 game.exe: $(SRCFILES)
 	gcc src/main.c $(CFLAGS) $(CLIBS) -o $@
 
 
 clean: 
-	rm game.exe
+	-rm game.exe
 
-
-.phony: all clean build
--- src/main.c
+++ src/main.c
@@ -1,3 +1,53 @@
+#include <SDL2/SDL.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+
+#define WINDOW_NAME "Metroidvania Jam 15"
+
+SDL_Window* global_window;
+bool running = true;
+
+static void initialize(void) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+}
+
+static void deinitialize(void) {
+    SDL_DestroyWindow(global_window);
+    SDL_Quit();
+}
+
+static void handle_sdl_event(SDL_Event event) {
+    switch (event.type) {
+        case SDL_QUIT: {
+            running = false;
+        } break;
+    }
+}
+
 int main(int argc, char** argv) {
+    initialize();
+
+    global_window = SDL_CreateWindow(
+        WINDOW_NAME,
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+    );
+
+    while (running) {
+        {
+            SDL_Event event;
+
+            while (SDL_PollEvent(&event)) {
+                handle_sdl_event(event);
+            }
+        }
+    }
+
+    deinitialize();
     return 0;
 }
