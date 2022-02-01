6c94dee48b06c0964369f90662e9e5ad2c96306a
Jerry Zhu <xpost2000cod@gmail.com>
2022-01-31 22:01:36 -0500

    Shuffling shit for later.

---
+++ src/binary_serializer.c
@@ -0,0 +1,15 @@
+enum binary_serializer_mode {
+    BINARY_SERIALIZER_READ,
+    BINARY_SERIALIZER_WRITE,
+};
+
+struct binary_serializer {
+    /*maybe use this arena, don't know quite yet*/
+    struct memory_arena arena;
+    /*or use the file, not too picky on sources*/
+    FILE* file_handle;
+
+    enum binary_serializer_mode mode;
+};
+
+/* TODO(jerry): tomorrow! */
